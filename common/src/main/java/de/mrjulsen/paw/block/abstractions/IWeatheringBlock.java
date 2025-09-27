f00fde998f86ecd7948746c2f57d024469908c90
MrJulsen <geisterdrache02@gmail.com>
2025-09-27 14:46:14 +0200

    Updated oxidation speed

--- common/src/main/java/de/mrjulsen/paw/block/abstractions/IWeatheringBlock.java
+++ common/src/main/java/de/mrjulsen/paw/block/abstractions/IWeatheringBlock.java
@@ -49,6 +49,6 @@ public interface IWeatheringBlock<T extends Block & IWeatheringBlock<T>> extends
     }
 
     default float getChanceModifier() {
-        return this.getAge() == WeatherState.UNAFFECTED ? 0.1F : 0.2F;
+        return this.getAge() == WeatherState.UNAFFECTED ? 0.2F : 0.3F;
     }
 }
