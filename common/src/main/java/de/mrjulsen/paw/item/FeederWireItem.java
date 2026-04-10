f30adde7801a77ba71f82dba84dfa7ba78b93559
MrJulsen <geisterdrache02@gmail.com>
2026-04-10 19:23:59 +0200

    [fixed] Power wires can no longer be connected to tensioning devices

--- common/src/main/java/de/mrjulsen/paw/item/FeederWireItem.java
+++ common/src/main/java/de/mrjulsen/paw/item/FeederWireItem.java
@@ -2,6 +2,7 @@ package de.mrjulsen.paw.item;
 
 import de.mrjulsen.paw.PantographsAndWires;
 import de.mrjulsen.paw.block.abstractions.AbstractCantileverBlock;
+import de.mrjulsen.paw.block.abstractions.ICatenaryWireConnector;
 import de.mrjulsen.paw.client.gui.ModGuiIcons;
 import de.mrjulsen.paw.data.WireHitResult;
 import de.mrjulsen.paw.registry.ModWireRegistry;
@@ -56,7 +57,7 @@ public class FeederWireItem implements IPawWireItemBase {
         } else if (hit instanceof RaycastHitResult h) {
             pos = h.getBlockPos();
         }
-        if (pos != null && level.getBlockEntity(pos) instanceof WireConnectorBlockEntity && !(level.getBlockState(pos).getBlock() instanceof AbstractCantileverBlock)) {
+        if (pos != null && level.getBlockEntity(pos) instanceof WireConnectorBlockEntity && !(level.getBlockState(pos).getBlock() instanceof ICatenaryWireConnector)) {
             return new BlockConnectorNodeData(pos);
         }
         return null;
